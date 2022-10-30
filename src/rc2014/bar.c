184e7164be614dc0df3af33839c87f47e5f683b4
Justin Skists <justin.skists@juzza.co.uk>
2022-10-30 16:24:16 +0000

    bar: make cursor display work!

--- src/rc2014/bar.c
+++ src/rc2014/bar.c
@@ -4,6 +4,7 @@
  */
 
 #include <stdbool.h>
+#include <conio.h>
 #include "bar.h"
 
 #define DISPLAY_ROWS 17
@@ -26,12 +27,7 @@ static unsigned char bar_y=3, bar_c=1, bar_m=1, bar_i=0, bar_oldi=0, bar_co=0x13
 
 void bar_clear(bool oldRow)
 {
-  // Fill column color in old row
-//  msx_vfill(ADDR_FILE_LIST + ROW(bar_y+(oldRow==true ? bar_oldi : bar_i)), ATTR_PATH_LINE, COL(bar_c));
-
-  // Fill background color in old row
-//  msx_vfill(ADDR_FILE_LIST + ROW(bar_y+(oldRow==true ? bar_oldi : bar_i))+COL(bar_c),ATTR_FILE_LIST,256-COL(bar_c));
-  cprintf("%c[%d;%dH ", 27, bar_y + (oldRow ? bar_oldi : bar_i), 1);
+  cprintf("%c[%d;%dH ", 27, bar_y + (oldRow ? bar_oldi : bar_i) + 1, 1);
 }
 
 /**
@@ -41,9 +37,7 @@ void bar_update(void)
 {
   bar_clear(true);
   
-  // Fill bar color in new row
-//  msx_vfill(ADDR_FILE_LIST + ROW(bar_y+bar_i),bar_co,256);
-  cprintf("%c[%d;%dH>", 27, bar_y + bar_i, 1);
+  cprintf("%c[%d;%dH>", 27, (bar_y + bar_i + 1), 1);
 }
 
 /**
